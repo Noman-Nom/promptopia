c2c269837fbb2cbffa5199e14df49e96f1bad5eb
Noman-Nom <noman5456318@gmail.com>
2024-07-26 16:05:49 +0500

    nav page working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,18 +11,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 
 const Nav = () => {
-     
-         const  isUserLoggedIn = true
+
+  const isUserLoggedIn = true
 
   return (
     <nav className='flex-between w-full mb-16 pt-3 '>
       <Link href='/' className='flex gap-2  flex-center'>
-        <Image 
-        src="/assets/images/logo.svg" 
-        alt="Promptopia"
-        width={30}
-        height={30}
-        className='object-contain'
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia"
+          width={30}
+          height={30}
+          className='object-contain'
         />
 
         <p className='logo_text'>Promptopia</p>
@@ -32,19 +32,32 @@ const Nav = () => {
 
       <div className='sm:flex hidden'>
 
-        {isUserLoggedIn ?(
+        {isUserLoggedIn ? (
 
           <div className='flex gap-3 md:gap-5'>
 
-              <Link href='/create-prompt'
+            <Link href='/create-prompt'
               className='black_btn'
-              >
-                Create Post
-              
-              </Link>
+            >
+              Create Post
+
+            </Link>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+              src='/assets/images/whiteBg.jpg'
+              alt='Profile'
+              width={37}
+              height={37}
+              className='rounded-full'
+              />
+            </Link>
           </div>
-        ):(<></>)
-      }
+        ) : (<></>)
+        }
       </div>
     </nav>
   )

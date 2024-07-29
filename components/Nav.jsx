a92aefde5bba1b86ccffa73b876bcc4292262681
Noman-Nom <noman5456318@gmail.com>
2024-07-29 12:05:42 +0500

    connected to mongoDB

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,7 +12,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
 
-  const isUserLoggedIn = true
+  const {data: session} = useSession()
 
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
@@ -42,12 +42,12 @@ const Nav = () => {
 
         <p className='logo_text'>Promptopia</p>
       </Link>
-
+{alert(session?.user)}
       {/* Desktop  Navigation */}
 
       <div className='sm:flex hidden'>
 
-        {isUserLoggedIn ? (
+        {session?.user ? (
 
           <div className='flex gap-3 md:gap-5'>
 
@@ -93,7 +93,7 @@ const Nav = () => {
 
       <div className='sm:hidden flex relative'>
         {
-          isUserLoggedIn ? (
+          session?.user ? (
           
           <div className='flex'>
                  <Image

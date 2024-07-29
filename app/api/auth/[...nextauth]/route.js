996cba934c1a7277625fa7664fc3d52163e57c7c
Noman-Nom <noman5456318@gmail.com>
2024-07-29 15:16:35 +0500

    auth done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,59 +1,48 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import env from 'dotenv'
-import User from "@models/user";
-import {connectToDB} from '@/utils/database'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import User from '@models/user';
+import { connectToDB } from '@/utils/database';
 
-
-console.log({
-    clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-})
 const handler = NextAuth({
-
-    providers:[
-        GoogleProvider({
-            clientId:'process.env.GOOGLE_ID',
-            clientSecret:'process.env.GOOGLE_CLIENT_SECRET',
-        })
-
-    ],
-    async session({session}){
-
-        const sessionUSer = await User.findOne({
-            email:session.user.email
-        })
-        session.user.id = sessionUSer._id.toString();
-        return session;
-
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
     },
-    async signIn({profile}){
-        try {
-            await connectToDB();
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-            //  check if the user already exists in the database
+        // Check if the user already exists in the database
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
 
-            const UserExists = await User.findOne({
-                email:profile.email
-            })
-
-            // if not ,  create a new user in the database
-            if(!UserExists){
-                await User.create({
-                    email:profile.email,
-                    username:profile.name.replace(" ", "").toLowerCase(),
-                    image:profile.picture
-                })
-            }
-
-
-            return true;
-        } catch (error) {
-            console.log(error)
-            return false;
+        // If not, create a new user in the database
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
         }
 
-    }
-})
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
 
-export {handler as GET , handler as POST}
+export { handler as GET, handler as POST };
--- utils/database.js
+++ utils/database.js
@@ -1,29 +1,25 @@
-import mongosse from 'mongoose';
+import mongoose from 'mongoose';
 
-let isConnected = false;  // track the connection
+let isConnected = false; // Track the connection
 
-export const connectToDB = async ()=>{
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
 
-    mongosse.set('strictQuery', true);
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-    if(isConnected){
-        console.log('MongoDB is already connected');
-        return;
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'shear_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    try {
-        await mongosse.connect(process.env.MONGODB_URI,{
-            dbName: 'shear_prompt',
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-
-        })
-
-        isConnected= true;
-        console.log('MongoDB is connected');
-    } catch (error) {
-        console.log(error)
-    }
-
-
-}
+    isConnected = true;
+    console.log('MongoDB is connected');
+  } catch (error) {
+    console.log(error);
+  }
+};

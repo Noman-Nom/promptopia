ee17dcfd277a14c71db2c165289a8f938bb19e04
Noman-Nom <noman5456318@gmail.com>
2024-07-31 16:23:32 +0500

    apis for prompt is created

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -9,19 +9,49 @@ import Form from '@components/Form'
 
 const CreatePrompt = () => {
 
-    const [submitting, setSubmitting] = useState(false)
 
+
+    const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({
         prompt:'',
         tag:'',
     })
+
+    const createPrompt = async (e) => {
+
+      e.preventDefault();
+      setSubmitting(true);
+
+      try{
+        const res = await fetch('api/prompt/new',{
+
+          method: 'POST',
+          body: JSON.stringify({
+            prompt:post.prompt,
+            userId:session?.user.id,
+            tag:post.tag
+          })
+        })
+        if(response.ok){
+            router.push('/');
+
+        }
+
+      } catch (error) {
+        console.error(error)
+      } finally{
+        setSubmitting(false)
+      }
+
+        
+    }
   return (
    <Form
    type='Create'
    post={post}
    setPost={setPost}
    submitting={submitting}
-   setSubmitting={setSubmitting}
+   handleSubmit={createPrompt}
 
    />
   )

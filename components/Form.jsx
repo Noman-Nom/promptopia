3f4c9f00350851728ffe153e41109fa031fc50e6
Noman-Nom <noman5456318@gmail.com>
2024-07-31 16:09:53 +0500

    changes

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Form = ({ type, post, setPost, submitting, setSubmitting }) => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <div className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
@@ -11,7 +11,7 @@ const Form = ({ type, post, setPost, submitting, setSubmitting }) => {
         imagination run wild with any AI-powered platform.
       </p>
       <form
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
         <label>
